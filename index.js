48664281c9842555020ff979c89ef549576754ba
Praneetha Charugundla <88439139+PraneethaCharugundla@users.noreply.github.com>
2022-09-16 18:17:11 +0530

    feat : added inde.js

---
+++ index.js
@@ -0,0 +1,88 @@
+var readlineSync = require("readline-sync");
+
+var score = 0;
+
+// data of high score
+var highScores = [
+  {
+    name: "Likhitha",
+    score: 3,
+  },
+
+  {
+    name: "Raj",
+    score: 2,
+  },
+]
+
+// array of objects
+var questions = [{
+  question: "Where do i live ? ",
+  answer: "vijayawada"
+
+},
+{
+  question: "Who is my favourite Hero?",
+  answer: "vijay"
+
+},
+{
+  question: "Where do i work?",
+  answer: "tcs"
+
+},
+{
+  question: "which is my favourite place to visit?",
+  answer: "kerala"
+
+},
+{
+  question: "Who is my favourite cricketer?",
+  answer: "virat kohli"
+
+}
+];
+
+function welcome() {
+  var userName = readlineSync.question("What's your name? ");
+
+  console.log("Welcome " + userName + " to DO YOU KNOW Tanay?");
+}
+
+
+// play function
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) { // branching
+    console.log("right!");
+    score = score + 1;
+
+  } else {
+    console.log("wrong!");
+
+  }
+
+  console.log("current score: ", score);
+  console.log("-------------")
+}
+
+function game() {
+  for (var i = 0; i < questions.length; i++) {
+    var currentQuestion = questions[i];
+    play(currentQuestion.question, currentQuestion.answer)
+  }
+}
+
+function showScores() {
+  console.log("YAY! You SCORED: ", score);
+
+  console.log("Check out the high scores, if you should be there ping me and I'll update it");
+
+  highScores.map(score => console.log(score.name, " : ", score.score))
+}
+
+
+welcome();
+game();
+showScores();
